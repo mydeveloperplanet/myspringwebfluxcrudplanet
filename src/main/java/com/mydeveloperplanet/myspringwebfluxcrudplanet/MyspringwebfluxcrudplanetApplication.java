b326a9ca0d2f484c3a13131418df7f45dd494624
Gunter Rotsaert <gunter@mydeveloperplanet.com>
2018-02-25 10:27:49 +0100

    Initial commit

---
+++ src/main/java/com/mydeveloperplanet/myspringwebfluxcrudplanet/MyspringwebfluxcrudplanetApplication.java
@@ -0,0 +1,12 @@
+package com.mydeveloperplanet.myspringwebfluxcrudplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringwebfluxcrudplanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringwebfluxcrudplanetApplication.class, args);
+	}
+}
